53f304eb44d10ac6db25338392c2630967dfc933
srletina <adresazasve@gmail.com>
2018-03-17 18:45:43 +0100

    WIP

--- src/ja/java8/StreamsTest.java
+++ src/ja/java8/StreamsTest.java
@@ -5,31 +5,32 @@ import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import com.sun.net.httpserver.Filter;
-
-@SuppressWarnings("unused")
 public class StreamsTest {
 
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Pero", "Lujo", "Maso", "Skrbo", "Pegla");
-		
+
 		list.stream()
-			.sorted()
-			.filter(s -> s.startsWith("P"))
-			.map(StringReverser::reverse)
-			.forEach(System.out::println);
-		
+		.sorted()
+		.filter(s -> s.startsWith("P"))
+		.map(StringReverser::reverse)
+		.forEach(System.out::println);
+		System.out.println();
+
 		boolean b = Stream.of("Pero", "Lujo", "Maso", "Skrbo", "Pegla")
-							//.anyMatch(s->s.startsWith("P"));
-							//.allMatch(String::isEmpty);
-							.noneMatch(s->s.length()>0);
+				//.anyMatch(s->s.startsWith("P"));
+				//.allMatch(String::isEmpty);
+				.noneMatch(s->s.length()>0);
 		System.out.println(b);
-		
+		System.out.println();
+		System.out.println();
+
 		long count = IntStream.range(1, 100)
-							  .filter(i->i%2==0)
-							  .count();
-		System.out.println("parnih do 100:" + count);
-		
+				.filter(i->i%2==0)
+				.count();
+		System.out.println("parnih do 100: " + count);
+		System.out.println();
+
 		IntStream.range(1, 10).reduce((x, y) -> x+y).ifPresent(System.out::print);
 	}
 }
