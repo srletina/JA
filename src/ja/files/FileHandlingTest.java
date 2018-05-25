2e3eae49df7b793976600c4c880f2134d8085629
srletina <adresazasve@gmail.com>
2018-05-25 19:13:38 +0200

    WIP

--- src/ja/files/FileHandlingTest.java
+++ src/ja/files/FileHandlingTest.java
@@ -8,10 +8,12 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.Writer;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+
 
 public class FileHandlingTest {
 
@@ -20,6 +22,7 @@ public class FileHandlingTest {
 		Path fileName2 = Paths.get("test2.txt");
 		Path fileName3 = Paths.get("/translatorResults/test2.txt");
 		Path fileName4 = Paths.get("translatorResults\\test2.txt/");
+		System.out.println(":: Filenames");
 		System.out.println(fileName);
 		System.out.println(fileName2);
 		System.out.println(fileName3);
@@ -30,6 +33,7 @@ public class FileHandlingTest {
 		Path path2 = Paths.get("C:/RAMT\\/dumps//////translatorResult/");
 		Path path3 = Paths.get("C:/RAMT\\dumps//////translatorResult/");
 		Path path4 = Paths.get("C:/RAMT\\dumps//////translatorResult/", "/translatorResults/test2.txt");
+		System.out.println(":: Paths");
 		System.out.println(path);
 		System.out.println(path2);
 		System.out.println(path3);
@@ -39,13 +43,22 @@ public class FileHandlingTest {
 		String targetString = path.resolve(fileName).toString();
 		Path targetPath = path.resolve(fileName2);
 		File file = new File(path.toString(), fileName.toString());
+		System.out.println(":: path.resolve & File(path, fileName)");
 		System.out.println(targetString);
 		System.out.println(targetPath);
 		System.out.println(file);
 		System.out.println();
 
-		String outString = "ajmo!";
+		URL url = FileHandlingTest.class.getClassLoader().getResource("");
+		URL url2 = FileHandlingTest.class.getClassLoader().getResource("studenti_dtd.xml");
+		URL url3 = FileHandlingTest.class.getClassLoader().getResource("resources/studenti_dtd.xml");
+		System.out.println(":: getResource");
+		System.out.println(url);
+		System.out.println(url2);
+		System.out.println(url3);
+		System.out.println();
 
+		String outString = "ajmo!";
 		RandomAccessFile fajlic = new RandomAccessFile(targetString, "rw");
 		fajlic.seek(fajlic.length());
 		//fajlic.writeBytes(outString);
